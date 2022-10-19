f72aab30937dd2587cf5014759c8a8aa22d80656
NicolasTheCat <97517397+NicolasTheCat@users.noreply.github.com>
2022-10-19 09:13:46 +0200

    Création espace test document
    Ajout d'un espace de test pour les documents sur la page profil

---
+++ app/src/main/java/com/example/doggo_android/views/DocumentMemberFragment.java
@@ -0,0 +1,34 @@
+package com.example.doggo_android.views;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.doggo_android.R;
+
+
+public class DocumentMemberFragment extends Fragment {
+
+
+    public DocumentMemberFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_document_member, container, false);
+    }
+}
--- app/src/main/java/com/example/doggo_android/views/ProfileFragment.java
+++ app/src/main/java/com/example/doggo_android/views/ProfileFragment.java
@@ -3,15 +3,20 @@ package com.example.doggo_android.views;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.doggo_android.R;
+import com.example.doggo_android.databinding.FragmentProfileBinding;
 
 
 public class ProfileFragment extends Fragment {
+
+    FragmentProfileBinding binding;
+
 
     public ProfileFragment() {
     }
@@ -25,6 +30,38 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        initDocumentButtons();
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+
+    //TODO: a enlever une fois les tests sur les documents finis
+    private void initDocumentButtons(){
+        binding.buttonMemberInterface.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_profileFragment_to_documentMemberFragment);
+            }
+        });
+
+        binding.buttonAdminInterface.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
